fe535752f3daf7412a0851ccb92ca38241ba8774
Juan Camilo González Jiménez <info@juancgonzalez.com>
2021-07-15 17:47:02 -0500

    👥 Instrucciones para uso del esquema

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import './scss/styles.scss';
 import { entrada, salida } from './utilidades/esquemas';
-console.log(entrada);
+
+// De momento las variables de entradas.fijos no las tratamos ya que son las que operan dentro del modelo. Se vuelven relevantes si pasamos el modelo a tensroflow.
+
+/**
+ * Debemos crear una interfaz para producir cada variable que debe ingresar el ususario
+ * Estas variables se encuentran en entrada.modificables
+ */
+console.log(entrada.modificables);
+
+/**
+ * Otra interfaz para mostrar los resultados de la predicción que hace el modelo
+ * Están en salida
+ */
 console.log(salida);
