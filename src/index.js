912895dec54f947eb0dfc382528dc8d01b83b49a
3st3r30sc0p10 <h.idarraga@uniandes.edu.co>
2021-08-11 10:11:53 -0500

    Adelantos colegios barra progreso botón desactivado

--- src/index.js
+++ src/index.js
@@ -6,7 +6,26 @@ const sumas = document.querySelectorAll('.sumar');
 const selecciones = document.querySelectorAll('.seleccion');
 const porcentaje = document.getElementById('porcentaje');
 const rangos = document.querySelectorAll('.uiRango');
-console.log(porcentaje, rangos);
+const n_teachers = document.getElementById('n_teachers');
+const n_teachers_vacc = document.getElementById('n_teachers_vacc');
+const n_school_going = document.getElementById('n_school_going');
+const n_classrooms = document.getElementById('n_classrooms');
+const classroom_size = document.getElementById('classroom_size');
+const school_type = document.getElementById('school_type');
+const height_room = document.getElementById('height_room');
+const width_room = document.getElementById('width_room');
+const length_room = document.getElementById('length_room');
+const masks_type = document.getElementById('masks_type');
+const ventilation_level = document.getElementById('ventilation_level');
+const class_duration = document.getElementById('class_duration');
+const informacionContextual = document.getElementById('informacionContextual');
+
+
+
+const botonEnviarDatos = document.getElementById('botonEnviarDatos');
+const porcentajeAvanzado = document.getElementById("porcentajeAvanzado");
+const entradas = document.querySelectorAll('.entrada');
+
 
 restas.forEach((btn) => {
   btn.onclick = (e) => {
@@ -26,12 +45,111 @@ rangos.forEach((elemento) => {
 
   rango.oninput = () => {
     valor.value = rango.value;
+    
   };
-
   valor.oninput = () => {
     rango.value = valor.value;
   };
 });
+
+//Actualizar porcentaje avanzado
+function actualizarBarraPorcentaje() {
+  const actives = document.querySelectorAll(".active");
+  let widthActual = ((actives.length) / (entradas.length)) * 100;
+  porcentajeAvanzado.style.width = widthActual + "%";
+  console.log(widthActual, entradas.length, actives.length);
+}
+
+//Event Listeners Click
+n_teachers.addEventListener('click', () => {
+  actualizarBarraPorcentaje();
+  n_teachers_vacc.classList.remove("hidden");
+  n_teachers_vacc.classList.add("active");
+})
+
+n_teachers_vacc.addEventListener('click', () => {
+  actualizarBarraPorcentaje();
+  n_school_going.classList.remove("hidden");
+  n_school_going.classList.add("active");
+})
+
+n_school_going.addEventListener('click', () => {
+  actualizarBarraPorcentaje();
+  n_classrooms.classList.remove("hidden");
+  n_classrooms.classList.add("active");
+})
+
+n_classrooms.addEventListener('click', () => {
+  actualizarBarraPorcentaje();
+  classroom_size.classList.remove("hidden");
+  classroom_size.classList.add("active");
+})
+
+classroom_size.addEventListener('click', () => {
+  actualizarBarraPorcentaje();
+  school_type.classList.remove("hidden");
+  school_type.classList.add("active");
+})
+
+school_type.addEventListener('click', () => {
+  actualizarBarraPorcentaje();
+  height_room.classList.remove("hidden");
+  height_room.classList.add("active");
+})
+
+height_room.addEventListener('click', () => {
+  actualizarBarraPorcentaje();
+  width_room.classList.remove("hidden");
+  width_room.classList.add("active");
+})
+
+width_room.addEventListener('click', () => {
+  actualizarBarraPorcentaje();
+  length_room.classList.remove("hidden");
+  length_room.classList.add("active");
+})
+
+length_room.addEventListener('click', () => {
+  actualizarBarraPorcentaje();
+  masks_type.classList.remove("hidden");
+  masks_type.classList.add("active");
+})
+
+masks_type.addEventListener('click', () => {
+  actualizarBarraPorcentaje();
+  ventilation_level.classList.remove("hidden");
+  ventilation_level.classList.add("active");
+})
+
+ventilation_level.addEventListener('click', () => {
+  actualizarBarraPorcentaje();
+  class_duration.classList.remove("hidden");
+  class_duration.classList.add("active");
+})
+
+class_duration.addEventListener('click', () => {
+  actualizarBarraPorcentaje();
+  botonEnviarDatos.classList.remove("hidden");
+})
+
+//Event listeners mouseover
+
+n_teachers.addEventListener('mouseover', () => {
+  informacionContextual.innerText = 'informacion contextual del numero de profesores'
+})
+
+n_teachers_vacc.addEventListener('mouseover', () => {
+  informacionContextual.innerText = 'Otras información diferente'
+})
+
+
+
+
+
+
+
+
+
 
 // const enviarNTeachers = document.getElementById('enviarNTeachers');
 // const enviarN_teachers_vacc = document.getElementById('enviarN_teachers_vacc');
@@ -49,7 +167,7 @@ rangos.forEach((elemento) => {
 // const canvas1 = document.getElementById('canvas1');
 // const ctx1 = canvas1.getContext('2d');
 
-// enviarN_teachers.addEventListener('click', () => {
+// enviarNteachers.addEventListener('click', () => {
 //   const n_teachersValue = document.getElementById('n_teachersValue').value;
 //   entrada.modificables.n_teachers.value = n_teachersValue;
 //   return false;
