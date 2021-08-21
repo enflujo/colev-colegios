a7f7d3ad412cd05d8cc17f5cc845bbd2dce95c60
3st3r30sc0p10 <h.idarraga@uniandes.edu.co>
2021-08-21 15:30:53 -0500

    Arreglo de la información contextual

--- src/utilidades/contextos.js
+++ src/utilidades/contextos.js
@@ -1,23 +1,41 @@
 export default {
-  n_teachers:
-    'El número aproximado de profesores/as que van a trabajar a tu colegio, teniendo en cuenta tanto a quienes trabajan en planta como a quienes van temporalmente a las instalaciones.',
-  n_teachers_vacc:
-    'Teniendo en cuenta la fase de vacunación y los datos aportados en el colegio, aproximadamente cuántos profesores/as deberían estar vacunados en el momento en el que respondes esta encuesta.',
-  n_school_going:
-    'A partir de número de estudiantes por cada salón y el número de salones o cursos, cuál es el aproximado de estudiantes que asisten a tu colegio.',
-  n_classrooms:
-    'El número de salones que son empleados diariamente, incluyendo salones en donde se realizan actividades extra-académicas.',
-  classroom_size:
-    'El número de estudiantes que asisten diariamente y que se reúnen en un mismo espacio durante toda la jornada escolar.',
-  school_type: 'El colegio es privado o es un colegio financiado por la nación o los entes territoriales.',
-  height_room: 'Cuántos metros aproximadamente tienen de alto los salones a los que asisten los estudiantes.',
-  width_room:
-    'La cantidad de metros aproximados contados desde la pared izquierda hasta la pared de la derecha en relación a la ubicación del tablero.',
-  length_room:
-    'La cantidad de metros aproximados contados desde la pared del frente hasta la pared de atrás en relación a la ubicación del tablero.',
+  n_teachers_preschool:
+    'El número aproximado de profesores/as de preescolar que van a trabajar a tu colegio, teniendo en cuenta tanto a quienes trabajan en planta como a quienes van temporalmente a las instalaciones.',
+  n_teachers_primary:
+    'El número aproximado de profesores/as de primaria que van a trabajar a tu colegio, teniendo en cuenta tanto a quienes trabajan en planta como a quienes van temporalmente a las instalaciones.',
+  n_teachers_highschool:
+    'El número aproximado de profesores/as de secundaria que van a trabajar a tu colegio, teniendo en cuenta tanto a quienes trabajan en planta como a quienes van temporalmente a las instalaciones.',
+  n_school_going_preschool:
+    'A partir de número de estudiantes por cada salón y el número de salones o cursos, cuál es el aproximado de estudiantes de preescolar que asisten a tu colegio.',
+  n_school_going_primary:
+    'A partir de número de estudiantes por cada salón y el número de salones o cursos, cuál es el aproximado de estudiantes de primaria que asisten a tu colegio.',
+  n_school_going_highschool:
+    'A partir de número de estudiantes por cada salón y el número de salones o cursos, cuál es el aproximado de estudiantes de secundaria que asisten a tu colegio.',
+  classroom_size_preschool:
+    'El número de estudiantes de preescolar que asisten diariamente y que se reúnen en un mismo espacio durante toda la jornada escolar.',
+  classroom_size_primary:
+    'El número de estudiantes de primaria que asisten diariamente y que se reúnen en un mismo espacio durante toda la jornada escolar.',
+  classroom_size_highschool:
+    'El número de estudiantes de secundaria que asisten diariamente y que se reúnen en un mismo espacio durante toda la jornada escolar.',
+  school_type: 'El colegio es privado o es financiado al 100% por el organismo público correspondiente.',
+  height_room_preschool: 'Cuántos metros aproximadamente tienen de alto los salones a los que asisten los estudiantes.',
+  height_room_primary: 'Cuántos metros aproximadamente tienen de alto los salones a los que asisten los estudiantes.',
+  height_room_highschool: 'Cuántos metros aproximadamente tienen de alto los salones a los que asisten los estudiantes.',
+  width_room_preschool:
+    'El número aproximado de metros contados desde la pared izquierda hasta la pared de la derecha en relación a la ubicación del tablero.',
+  width_room_primary:
+    'El número aproximado de metros contados desde la pared izquierda hasta la pared de la derecha en relación a la ubicación del tablero.',
+  width_room_highschool:
+    'El número aproximado de metros contados desde la pared izquierda hasta la pared de la derecha en relación a la ubicación del tablero.',
+  length_room_preschool:
+    'El número aproximado de metros contados desde la pared del frente hasta la pared de atrás en relación a la ubicación del tablero.',
+  length_room_primary:
+    'El número aproximado de metros contados desde la pared del frente hasta la pared de atrás en relación a la ubicación del tablero.',
+  length_room_highschool:
+    'El número aproximado de metros contados desde la pared del frente hasta la pared de atrás en relación a la ubicación del tablero.',
   masks_type: 'Cuál es el material y las especificaciones del tapabocas que empleas para asistir a lugares públicos.',
   ventilation_level:
-    'Dependiendo del número de ventanas, el alto y ancho de las mismas y la cantidad de ellas que pueden abrirse, indica un nivel aproximado de ventilación en el salón de clase.',
+    'Dependiendo de si las ventanas y puertas del salón permanecen abiertas, y de si el salón está ubicado en un lugar por donde pasa el viento, se medirá el nivel de ventilación.',
   class_duration:
-    'La duración de la jornada escolar, descontando aquellas horas donde se realizan actividades al aire libre..',
+    'La cantidad de horas de clase al interior del salón, descontando el descanso, las comidas o las actividaees físicas que se realizan por fuera del mismo.',
 };
