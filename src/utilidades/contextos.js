c33853549d1f357543ff67247a3961004013aab4
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2021-10-05 10:35:00 -0500

    📝 Actualizar contextos

--- src/utilidades/contextos.js
+++ src/utilidades/contextos.js
@@ -1,41 +1,46 @@
 export default {
   n_teachers_preschool:
-    'El cuerpo docente incluye tanto a quienes asisten presencialmente durante toda la jornada escolar como a quienes  asisten por periodos más cortos. Este dato determina el impacto sobre la reproducción de contagios en tanto que es una población que tiene contacto con diferentes personas del colegio, incluyendo a sus trabajadores y directivas.',
+    'El cuerpo docente incluye tanto a quienes asisten presencialmente durante toda la jornada escolar como a quienes asisten por periodos más cortos. Los profesores tienen contacto directo con diferentes personas del colegio, incluyendo a sus trabajadores y directivas, de esta manera, este dato influye directamente en la reproducción del virus.',
   n_teachers_primary:
-    'El cuerpo docente incluye tanto a quienes asisten presencialmente durante toda la jornada escolar como a quienes  asisten por periodos más cortos. Este dato determina el impacto sobre la reproducción de contagios en tanto que es una población que tiene contacto con diferentes personas del colegio, incluyendo a sus trabajadores y directivas.',
+    'El cuerpo docente incluye tanto a quienes asisten presencialmente durante toda la jornada escolar como a quienes asisten por periodos más cortos. Los profesores tienen contacto directo con diferentes personas del colegio, incluyendo a sus trabajadores y directivas, de esta manera, este dato influye directamente en la reproducción del virus.',
   n_teachers_highschool:
-    'El cuerpo docente incluye tanto a quienes asisten presencialmente durante toda la jornada escolar como a quienes  asisten por periodos más cortos. Este dato determina el impacto sobre la reproducción de contagios en tanto que es una población que tiene contacto con diferentes personas del colegio, incluyendo a sus trabajadores y directivas.',
+    'El cuerpo docente incluye tanto a quienes asisten presencialmente durante toda la jornada escolar como a quienes asisten por periodos más cortos. Los profesores tienen contacto directo con diferentes personas del colegio, incluyendo a sus trabajadores y directivas, de esta manera, este dato influye directamente en la reproducción del virus.',
   n_school_going_preschool:
-    'El número total de estudiantes por nivel educativo determina la cantidad de contactos personales posibles entre quienes asisten presencialmente al colegio. Esta cifra permite calcular las interacciones aisladas que podría tener cada estudiante.',
+    'El número total de estudiantes por nivel educativo nos ayuda a estimar la cantidad de contactos posibles entre quienes asisten presencialmente al colegio. Esta cifra permite calcular las interacciones aisladas que podría tener cada estudiante.',
   n_school_going_primary:
-    'El número total de estudiantes por nivel educativo determina la cantidad de contactos personales posibles entre quienes asisten presencialmente al colegio. Esta cifra permite calcular las interacciones aisladas que podría tener cada estudiante.',
+    'El número total de estudiantes por nivel educativo nos ayuda a estimar la cantidad de contactos posibles entre quienes asisten presencialmente al colegio. Esta cifra permite calcular las interacciones aisladas que podría tener cada estudiante.',
   n_school_going_highschool:
-    'El número total de estudiantes por nivel educativo determina la cantidad de contactos personales posibles entre quienes asisten presencialmente al colegio. Esta cifra permite calcular las interacciones aisladas que podría tener cada estudiante.',
+    'El número total de estudiantes por nivel educativo nos ayuda a estimar la cantidad de contactos posibles entre quienes asisten presencialmente al colegio. Esta cifra permite calcular las interacciones aisladas que podría tener cada estudiante.',
   classroom_size_preschool:
-    'El número de estudiantes que comparten un mismo espacio por un periodo prolongado, con una interacción cercana y frecuente, influye de manera significativa en los cálculos del modelo, ya que este número de personas son las que más probabilidades tienen de reproducir el contagio.',
+    'El número de estudiantes que comparten un mismo espacio por un periodo prolongado, con una interacción cercana y frecuente. ¡Este dato es crucial! Ya que este número de personas son las que más probabilidades tienen de reproducir el contagio.',
   classroom_size_primary:
-    'El número de estudiantes que comparten un mismo espacio por un periodo prolongado, con una interacción cercana y frecuente, influye de manera significativa en los cálculos del modelo, ya que este número de personas son las que más probabilidades tienen de reproducir el contagio.',
+    'El número de estudiantes que comparten un mismo espacio por un periodo prolongado, con una interacción cercana y frecuente. ¡Este dato es crucial! Ya que este número de personas son las que más probabilidades tienen de reproducir el contagio.',
   classroom_size_highschool:
-    'El número de estudiantes que comparten un mismo espacio por un periodo prolongado, con una interacción cercana y frecuente, influye de manera significativa en los cálculos del modelo, ya que este número de personas son las que más probabilidades tienen de reproducir el contagio.',
-  school_type: 'El colegio es privado o es financiado al 100% por el organismo público correspondiente. Este dato adquiere relevancia en análisis epidemiológicos que incluyen variables socio-económicas',
-  height_room_preschool: 'Las dimensiones de cada salón según el nivel educativo permiten calcular la concentración de las particular que viajan por el aire en un espacio cerrado. Estos valores se calculan junto al número de estudiantes por salón y el nivel de ventilación, determinando la posible concentración del virus y su transmisión por aerosoles.',
-  height_room_primary: 'Las dimensiones de cada salón según el nivel educativo permiten calcular la concentración de las particular que viajan por el aire en un espacio cerrado. Estos valores se calculan junto al número de estudiantes por salón y el nivel de ventilación, determinando la posible concentración del virus y su transmisión por aerosoles.',
-  height_room_highschool: 'Las dimensiones de cada salón según el nivel educativo permiten calcular la concentración de las particular que viajan por el aire en un espacio cerrado. Estos valores se calculan junto al número de estudiantes por salón y el nivel de ventilación, determinando la posible concentración del virus y su transmisión por aerosoles.',
+    'El número de estudiantes que comparten un mismo espacio por un periodo prolongado, con una interacción cercana y frecuente. ¡Este dato es crucial! Ya que este número de personas son las que más probabilidades tienen de reproducir el contagio.',
+  school_type:
+    'El colegio es privado o es financiado al 100% por el organismo público correspondiente. Este dato adquiere relevancia en análisis epidemiológicos que incluyen variables socio-económicas',
+  height_room_preschool:
+    'Las dimensiones de cada salón permiten calcular la concentración de aerosoles que viajan por el aire en un espacio cerrado. Estos valores utilizan junto al número de estudiantes por salón y el nivel de ventilación para calcular la posible concentración del virus en el aire.',
+  height_room_primary:
+    'Las dimensiones de cada salón permiten calcular la concentración de aerosoles que viajan por el aire en un espacio cerrado. Estos valores utilizan junto al número de estudiantes por salón y el nivel de ventilación para calcular la posible concentración del virus en el aire.',
+  height_room_highschool:
+    'Las dimensiones de cada salón permiten calcular la concentración de aerosoles que viajan por el aire en un espacio cerrado. Estos valores utilizan junto al número de estudiantes por salón y el nivel de ventilación para calcular la posible concentración del virus en el aire.',
   width_room_preschool:
-    'Las dimensiones de cada salón según el nivel educativo permiten calcular la concentración de las particular que viajan por el aire en un espacio cerrado. Estos valores se calculan junto al número de estudiantes por salón y el nivel de ventilación, determinando la posible concentración del virus y su transmisión por aerosoles.',
+    'Las dimensiones de cada salón permiten calcular la concentración de aerosoles que viajan por el aire en un espacio cerrado. Estos valores utilizan junto al número de estudiantes por salón y el nivel de ventilación para calcular la posible concentración del virus en el aire.',
   width_room_primary:
-    'Las dimensiones de cada salón según el nivel educativo permiten calcular la concentración de las particular que viajan por el aire en un espacio cerrado. Estos valores se calculan junto al número de estudiantes por salón y el nivel de ventilación, determinando la posible concentración del virus y su transmisión por aerosoles.',
+    'Las dimensiones de cada salón permiten calcular la concentración de aerosoles que viajan por el aire en un espacio cerrado. Estos valores utilizan junto al número de estudiantes por salón y el nivel de ventilación para calcular la posible concentración del virus en el aire.',
   width_room_highschool:
-    'Las dimensiones de cada salón según el nivel educativo permiten calcular la concentración de las particular que viajan por el aire en un espacio cerrado. Estos valores se calculan junto al número de estudiantes por salón y el nivel de ventilación, determinando la posible concentración del virus y su transmisión por aerosoles.',
+    'Las dimensiones de cada salón permiten calcular la concentración de aerosoles que viajan por el aire en un espacio cerrado. Estos valores utilizan junto al número de estudiantes por salón y el nivel de ventilación para calcular la posible concentración del virus en el aire.',
   length_room_preschool:
-    'Las dimensiones de cada salón según el nivel educativo permiten calcular la concentración de las particular que viajan por el aire en un espacio cerrado. Estos valores se calculan junto al número de estudiantes por salón y el nivel de ventilación, determinando la posible concentración del virus y su transmisión por aerosoles.',
+    'Las dimensiones de cada salón permiten calcular la concentración de aerosoles que viajan por el aire en un espacio cerrado. Estos valores utilizan junto al número de estudiantes por salón y el nivel de ventilación para calcular la posible concentración del virus en el aire.',
   length_room_primary:
-    'Las dimensiones de cada salón según el nivel educativo permiten calcular la concentración de las particular que viajan por el aire en un espacio cerrado. Estos valores se calculan junto al número de estudiantes por salón y el nivel de ventilación, determinando la posible concentración del virus y su transmisión por aerosoles.',
+    'Las dimensiones de cada salón permiten calcular la concentración de aerosoles que viajan por el aire en un espacio cerrado. Estos valores utilizan junto al número de estudiantes por salón y el nivel de ventilación para calcular la posible concentración del virus en el aire.',
   length_room_highschool:
-    'Las dimensiones de cada salón según el nivel educativo permiten calcular la concentración de las particular que viajan por el aire en un espacio cerrado. Estos valores se calculan junto al número de estudiantes por salón y el nivel de ventilación, determinando la posible concentración del virus y su transmisión por aerosoles.',
-  masks_type: 'El tipo de mascarilla que utilizan las personas que asisten presencialmente determina la cantidad de particular que una persona puede esparcir en un espacio cerrado. Estos datos se calculan junto a las dimensiones del salón, el número de personas en cada uno de ellos y la duración de tiempo en el que se comparten estos espacios.',
+    'Las dimensiones de cada salón permiten calcular la concentración de aerosoles que viajan por el aire en un espacio cerrado. Estos valores utilizan junto al número de estudiantes por salón y el nivel de ventilación para calcular la posible concentración del virus en el aire.',
+  masks_type:
+    'El tipo de mascarilla que utilizan las personas que asisten presencialmente determina la cantidad de partículas (aerosoles) que una persona puede esparcir en un espacio cerrado. Estos datos se utilizan junto con las dimensiones del salón, el número de personas en cada uno de ellos y la duración clases para determinar la exposición a los aerosoles.',
   ventilation_level:
-    'Dependiendo de si las ventanas y puertas del salón permanecen abiertas, y de si el salón está ubicado en un lugar por donde pasa el viento, se medirá el nivel de ventilación. La ventilación es un factor importante para calcular la cantidad de particulas que pueden concentrase en un espacio cerrado durante un intervalo de tiempo determinado, a la luz de las personas que comparten este espacio e interactúan durante este lapso de tiempo.',
+    'La ventilación es un factor importante para calcular la cantidad de partículas que pueden concentrase en un espacio cerrado durante un tiempo determinado.',
   class_duration:
-    'La cantidad de horas que estudiantes y cuerpo docente comparte determina la acumulación de particular que pueden llegar a concentrarse en un espacio cerrado. Este dato se calcula junto al número de estudiantes, las dimensiones de los salones y el nivel de ventilación para determinar el nivel de riesgo de cada individuo que hace parte de las interacciones que ocurren en los salones.',
+    'La cantidad de horas que estudiantes y cuerpo docente comparte en un espacio cerrado, nos ayuda a entender la duración de la exposición.'
 };
